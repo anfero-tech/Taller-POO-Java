36fc9a160b79a0080ac3db949872d0ea644a894e
Andres Rodriguez <anfero969@gmail.com>
2026-04-05 18:23:55 -0500

    feat(museo): implementacion de Obra con validacion de integridad cronologica

--- src/Museo/Obra.java
+++ src/Museo/Obra.java
@@ -9,13 +9,20 @@ public class Obra {
     public Obra(String titulo, String autor, int anio, String tipo) {
         this.titulo = titulo;
         this.autor = autor;
-        this.anio = anio;
+        setAnio(anio); // Validación
         this.tipo = tipo;
     }
 
+    public void setAnio(int anio) {
+        if (anio <= 2026) { this.anio = anio; }
+        else { System.out.println("Error: Año no válido."); }
+    }
+
+    public String getAutor() { return autor; }
+    public String getTitulo() { return titulo; }
+
     @Override
     public String toString() {
-        return String.format("Obra: %-20s | Autor: %-15s | Año: %d | Tipo: %s", 
-                             titulo, autor, anio, tipo);
+        return String.format("[%s] %-15s - %-15s (%d)", tipo, titulo, autor, anio);
     }
 }
