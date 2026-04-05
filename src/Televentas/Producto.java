89149d663152d95790baa25f45b63d98c4beae89
Andres Rodriguez <anfero969@gmail.com>
2026-04-05 17:37:13 -0500

    feat: implementacion de getters/setters con validaciones de negocio

--- src/Televentas/Producto.java
+++ src/Televentas/Producto.java
@@ -13,3 +13,21 @@ public class Producto {
         this.stock = stock;
     }
 }
+
+public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+    public int getStock() { return stock; }
+
+    public void setPrecio(double precio) {
+        if (precio >= 0) {
+            this.precio = precio;
+        } else {
+            System.out.println("Error: Precio inválido.");
+        }
+    }
+
+    public void setStock(int stock) {
+        if (stock >= 0) {
+            this.stock = stock;
+        }
+    }
