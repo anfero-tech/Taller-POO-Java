278730e4c03a98c0d5938668280916d47994c155
Andres Rodriguez <anfero969@gmail.com>
2026-04-05 17:51:51 -0500

    feat: flujo completo de venta y generacion de factura en Main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@ import Televentas.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Creación de Inventario (Objetos Producto)
+
         Producto p1 = new Producto("DS01", "Laptop Gamer", 4500000.0, 5);
         Producto p2 = new Producto("DS02", "Monitor 4K", 1800000.0, 8);
         Producto p3 = new Producto("DS03", "Mouse Optico", 85000.0, 20);
 
-        // 2. Creación del Pedido
+        
         Pedido miPedido = new Pedido(1001, "Andres Rodriguez");
+
+        System.out.println("--- INICIANDO PROCESO DE VENTA ---");
+        miPedido.agregarProducto(p1, 1);
+        miPedido.agregarProducto(p3, 2);
+        
+    
+        miPedido.agregarProducto(p2, 10); 
+
+        miPedido.generarFactura();
     }
 }
