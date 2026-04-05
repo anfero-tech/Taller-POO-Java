12a0f04646f4e0a323a41be3c86c6bf0b4fed512
Andres Rodriguez <anfero969@gmail.com>
2026-04-05 18:22:00 -0500

    final: integracion total de modulos de taller

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import Televentas.*;
+import Museo.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Ejecución Televentas
+        System.out.println(">>> EJECUTANDO MODULO TELEVENTAS <<<");
+        Producto p1 = new Producto("TV01", "Laptop Gamer", 4500000, 5);
+        Pedido ped = new Pedido(1001, "Andres Rodriguez");
+        ped.agregarProducto(p1, 1);
+        ped.generarFactura();
 
-        Producto p1 = new Producto("DS01", "Laptop Gamer", 4500000.0, 5);
-        Producto p2 = new Producto("DS02", "Monitor 4K", 1800000.0, 8);
-        Producto p3 = new Producto("DS03", "Mouse Optico", 85000.0, 20);
-
-        
-        Pedido miPedido = new Pedido(1001, "Andres Rodriguez");
-
-        System.out.println("--- INICIANDO PROCESO DE VENTA ---");
-        miPedido.agregarProducto(p1, 1);
-        miPedido.agregarProducto(p3, 2);
-        
-    
-        miPedido.agregarProducto(p2, 10); 
-
-        miPedido.generarFactura();
+        // Ejecución Museo
+        System.out.println("\n>>> EJECUTANDO MODULO MUSEO <<<");
+        Catalogo miMuseo = new Catalogo("Museo de La Salle");
+        miMuseo.registrarObra(new Obra("La Mona Lisa", "Da Vinci", 1503, "Pintura"));
+        miMuseo.mostrarInventario();
     }
 }
